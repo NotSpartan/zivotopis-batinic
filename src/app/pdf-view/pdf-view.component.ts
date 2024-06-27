afdde4701ce07d2cfa1ade54ad009c77613d6891
NotSpartan <josip.batinic9@gmail.com>
2024-06-27 18:58:37 +0200

    dorada pdf generiranja

--- src/app/pdf-view/pdf-view.component.ts
+++ src/app/pdf-view/pdf-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, signal, WritableSignal  } from '@angular/core';
+import { Component, Input, signal, WritableSignal, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OsobniPodaciComponent } from '../osobni-podaci/osobni-podaci.component';
 import { CiljeviMotivacijaComponent } from '../ciljevi-motivacija/ciljevi-motivacija.component';
@@ -6,6 +6,8 @@ import { IskustvoComponent } from '../iskustvo/iskustvo.component';
 import { ObrazovanjeComponent } from '../obrazovanje/obrazovanje.component';
 import { TehnoloskiStackComponent } from '../tehnoloski-stack/tehnoloski-stack.component';
 import { ProfileHeaderComponent } from '../profile-header/profile-header.component';
+import html2canvas from 'html2canvas';
+import { jsPDF } from 'jspdf';
 
 @Component({
   selector: 'app-pdf-view',
@@ -33,4 +35,36 @@ export class PdfViewComponent {
 
   _imePrezime: WritableSignal<string> = signal('');
   _titula: WritableSignal<string> = signal('');
+
+  @ViewChild('pdfContainer') pdfContainer!: ElementRef;
+
+  async generatePDF() {
+    const element = this.pdfContainer.nativeElement;
+    const pdf = new jsPDF('p', 'mm', 'a4');
+    const pdfWidth = pdf.internal.pageSize.getWidth();
+    const pdfHeight = pdf.internal.pageSize.getHeight();
+    
+    const pageElements = element.querySelectorAll('.pdf-page');
+    for (let i = 0; i < pageElements.length; i++) {
+      const pageElement = pageElements[i] as HTMLElement;
+      const canvas = await html2canvas(pageElement, {
+        scale: 1.2,
+        useCORS: true,
+        logging: false
+      });
+
+      const imgData = canvas.toDataURL('image/jpeg', 1.0);
+      
+      const imgWidth = pdfWidth;
+      const imgHeight = canvas.height * imgWidth / canvas.width;
+      
+      if (i > 0) {
+        pdf.addPage();
+      }
+
+      pdf.addImage(imgData, 'JPEG', 0, 0, imgWidth, imgHeight);
+    }
+
+    pdf.save('zivotopis.pdf');
+  }
 }
