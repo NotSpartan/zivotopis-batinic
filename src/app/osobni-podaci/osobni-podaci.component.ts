32e1b690395152f255523ee697e24a8d74d07c22
NotSpartan <josip.batinic9@gmail.com>
2024-06-28 10:08:17 +0200

    dorada - labela za pola u komponenti osobni-podatci

--- src/app/osobni-podaci/osobni-podaci.component.ts
+++ src/app/osobni-podaci/osobni-podaci.component.ts
@@ -48,10 +48,10 @@ export class OsobniPodaciComponent {
   isAuthor = computed(() => this.authService.isAuthor());
 
   fields = computed<Field[]>(() => [
-    { name: 'imePrezime', icon: 'person', value: this.imePrezime, type: 'text', placeholder: 'Ime i prezime' },
-    { name: 'titula', icon: 'work', value: this.titula, type: 'text', placeholder: 'Zanimanje' },
-    { name: 'email', icon: 'email', value: this.email, type: 'email', placeholder: 'Email' },
-    { name: 'telefon', icon: 'phone', value: this.telefon, type: 'tel', placeholder: 'Telefon' },
+    { name: 'imePrezime', icon: 'Osoba', value: this.imePrezime, type: 'text', placeholder: 'Ime i prezime' },
+    { name: 'titula', icon: 'Pozicija', value: this.titula, type: 'text', placeholder: 'Zanimanje' },
+    { name: 'email', icon: 'E-pošta', value: this.email, type: 'email', placeholder: 'Email' },
+    { name: 'telefon', icon: 'Telefon', value: this.telefon, type: 'tel', placeholder: 'Telefon' },
   ]);
 
   ngOnInit() {
